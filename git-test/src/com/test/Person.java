a69c9da9dc8f5a2e2bc9d5d697d539ce91a32772
RuXingGit <644739980@qq.com>
2020-04-09 12:15:20 +0800

    对person修改

--- git-test/src/com/test/Person.java
+++ git-test/src/com/test/Person.java
@@ -7,4 +7,20 @@ package com.test;
 public class Person {
     private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
